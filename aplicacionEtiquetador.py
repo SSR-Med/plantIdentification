9efa0c511e67664f99e7fb0e5fbfdfaee43130e2
SSR-Med <sasalazarr@unal.edu.co>
2022-07-15 12:11:23 -0500

    Label per Folder
    There is only one job left: let YoloV5 create a model with 2000 green image plant (Let's see how this will be going)

--- aplicacionEtiquetador.py
+++ aplicacionEtiquetador.py
@@ -1,5 +1,4 @@
 from tkinter import Tk     # from tkinter import Tk for Python 3.x
-from tkinter.filedialog import askopenfilename
 from tkinter.filedialog import askdirectory
 import tkinter as tk
 from tkinter import messagebox
@@ -7,15 +6,21 @@ import os
 import time
 Tk().withdraw()  # we don't want a full GUI, so keep the root window from appearing
 # show an "Open" dialog box and return the path to the selected file
-print("Select your image/video")
+print("Select the folder of your images")
 time.sleep(2)
-filename = str(askopenfilename())
-print("Select the folder where you want to save your file")
+directoryImages = str(askdirectory())
+dir_list = os.listdir(directoryImages)
+print("Select the folder where you want to save your label.txt")
 time.sleep(2)
 directory = str(askdirectory())
-if filename[-3:] in {'jpg', 'jpeg', 'png'}:
-    from Etiquetador import creadorEtiqueta
-    Etiqueta = creadorEtiqueta(filename, directory)
-    # EN caso de que no haya ninguna etiqueta en la imagen entonces se borra la imagen
-    if Etiqueta == False:
+for img in dir_list:
+    filename = "{direccion}/{imagen}".format(
+        direccion=directoryImages, imagen=str(img))
+    if filename[-3:] in {'jpg', 'jpeg', 'png'}:
+        from Etiquetador import creadorEtiqueta
+        Etiqueta = creadorEtiqueta(filename, directory)
+        # EN caso de que no haya ninguna etiqueta en la imagen entonces se borra la imagen
+        if Etiqueta == False:
+            os.remove(filename)
+    else:
         os.remove(filename)
